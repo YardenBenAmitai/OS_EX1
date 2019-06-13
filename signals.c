72aedb8f9b3b51526278675097829bfce8fc661e
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-06-13 20:00:13 +0300

    Add files via upload

---
+++ signals.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int pid_list[5];
+int indx;
+
+void sigCatcher();  //declaration of function
+
+
+int main() {
+	pid_t pid, finished;
+	int status;
+	
+	signal(SIGINT, sigCatcher);
+	
+	for (int i=0; i<5; i++){
+		pid=fork();
+		if (pid<0){
+			perror("fork malfuctioned");
+		} else if(pid>0){
+			pid_list[i] = pid;
+		} else{
+			printf("PID %d ready\n", getpid());
+			indx=i-1;
+			pause();
+			exit(0);
+		}
+	}
+	
+	sleep(1);
+	
+	kill(pid_list[4], SIGINT);
+	for (int i=0; i<5; i++){
+		finished=wait(&status);
+		printf("Process %d is dead\n", finished);
+	}
+	exit(0);
+}
+
+void sigCatcher(){
+	//printf("inside sigCatcher\n");
+	printf("PID %d caught one\n", getpid());
+	signal(SIGINT, sigCatcher);
+	if (indx!=-1){
+		kill(pid_list[indx], SIGINT);
+	}
+}
